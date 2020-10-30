0e88db02e2e88bdcecddd1d462acd27e6ba38eda
ZuuZaa <zumbaghirova@gmail.com>
2020-10-30 13:34:38 +0400

    index

--- blog/views.py
+++ blog/views.py
@@ -3,23 +3,24 @@ from blog.models import BlogModel
 # Create your views here.
 
 def blog_view(request):
-    try:
-        blog_list = BlogModel.objects.all()
-    except BlogModel.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    # try:
+    #     blog_list = BlogModel.objects.all()
+    # except BlogModel.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+    blog_list = BlogModel.objects.all()
+    context = {
+        'blog_list': blog_list
+    }
+    return render(request, "blog_list.html", context)
 
-    if request.method == 'GET':
-        return render(request, "blog_list.html")
-    else:
-        pass
 
 def detail_blog_view(request, id):
-    try:
-        blog_post = BlogModel.objects.get(id=id)
-    except BlogModel.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    # try:
+    #     blog_post = BlogModel.objects.get(id=id)
+    # except BlogModel.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        return render(request, "blog_detail.html", {'blog_post':'blog_post'})
-    else:
-        pass
+    # if request.method == 'GET':
+    #     return render(request, "blog_detail.html", {'blog_post':'blog_post'})
+    # else:
+    pass
