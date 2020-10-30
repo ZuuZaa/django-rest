a0a1b52c337c842ba1ee93d35692d0cebab0a618
ZuuZaa <zumbaghirova@gmail.com>
2020-10-30 13:43:11 +0400

    static

--- REST/settings.py
+++ REST/settings.py
@@ -139,7 +139,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_HOST = 'https://myblogpagefortesting.herokuapp.com/' if not DEBUG else ''
 STATIC_URL = STATIC_HOST + '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
